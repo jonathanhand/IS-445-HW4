810695d25532f73a6b6e9055c303e68549331071
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-13 21:27:18 -0700

    formatting

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ function loadDoc(url, cFunction) {
 function emailSplit(xhttp) {
   const recDataOld = JSON.parse(xhttp.responseText); //sets response as constant
   var emailList = [];
-  for (property in recDataOld) { //loop through data
+  for (property in recDataOld) {
+    //loop through data
     emailList.push(recDataOld[property].email); //takes out each email and adds to list
   }
   emailList = emailList.sort(); //sorts alphabetically
@@ -25,21 +26,22 @@ function emailSplit(xhttp) {
 }
 
 //fetch
-fetch('https://jsonplaceholder.typicode.com/users') //path we are fetching
-    .then(function(response) { //promise
-        return response.json(); //returns promise containing the response object
-    })
-    .then(function(myJson) {
-        usernameList = [];
-        //const recDataNew = JSON.parse(myJson);
-        for (i in myJson) { //loops through response data
-        usernameList.push(myJson[i].username); //add to list
-        }
-        usernameList.sort(function(a,b){ //sorts by length of string
-            return a.length - b.length;
-        });
-        for (name in usernameList)
-        document.getElementById("box2").innerHTML += usernameList[name] + "</br>"; //add to box2 div
-    
+fetch("https://jsonplaceholder.typicode.com/users") //path we are fetching
+  .then(function(response) {
+    //promise
+    return response.json(); //returns promise containing the response object
+  })
+  .then(function(myJson) {
+    usernameList = [];
+    //const recDataNew = JSON.parse(myJson);
+    for (i in myJson) {
+      //loops through response data
+      usernameList.push(myJson[i].username); //add to list
+    }
+    usernameList.sort(function(a, b) {
+      //sorts by length of string
+      return a.length - b.length;
     });
-
+    for (name in usernameList)
+      document.getElementById("box2").innerHTML += usernameList[name] + "</br>"; //add to box2 div
+  });
