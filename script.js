41c1ac545406030ddc50029a92e021c8c6bfffbe
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-13 17:34:54 -0700

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.open('GET','https://jsonplaceholder.typicode.com/users', true);
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        const recData = JSON.parse(xhttp.responseText);
+       // Typical action to be performed when the document is ready:
+       var emailList = []
+       for (property in recData) {
+           emailList.push(recData[property].email);
+           emailList.sort;
+           console.log(emailList);
+       }
+       for (entry in emailList)
+        document.getElementById("box1").innerHTML += emailList[entry] + '</br>';
+
+    }
+};
+xhttp.send();
