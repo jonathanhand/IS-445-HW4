3489d0693be1bbbde9378286ff6528c79638639a
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-13 20:49:33 -0700

    changed to callback method for xmlhttp request

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-var xhttp = new XMLHttpRequest();
+loadDoc("https://jsonplaceholder.typicode.com/users", emailSplit)
+function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+    }
+};
+xhttp.open("GET", url, true);
+xhttp.send();
+}
 
-xhttp.open('GET','https://jsonplaceholder.typicode.com/users', true);
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        const recData = JSON.parse(xhttp.responseText);
-       // Typical action to be performed when the document is ready:
-       var emailList = []
+function emailSplit(xhttp) {
+    const recData = JSON.parse(xhttp.responseText);
+    var emailList = [];
+    for (property in recData) {
+        emailList.push(recData[property].email);
+        emailList.sort;
+    }
+    for (entry in emailList) {
+    document.getElementById("box1").innerHTML += emailList[entry] + '</br>';
+}
+}
+    /*
+    var emailList = []
        for (property in recData) {
            emailList.push(recData[property].email);
            emailList.sort;
            console.log(emailList);
        }
-       for (entry in emailList)
-        document.getElementById("box1").innerHTML += emailList[entry] + '</br>';
+       for (entry in emailList) {
+        document.getElementById("box1").innerHTML = xttp.responseText;//+= emailList[entry] + '</br>';
     }
-};
-xhttp.send();
+}
+*/
+/*
+//fetch
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    })
+*/
